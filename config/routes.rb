9ef2cd34c424d99a6873df167aaa5098480b589e
nauticalnova <nauticalnova@hotmail.com>
2017-05-18 21:19:24 -0600

    added basic structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
+  get 'videos/index'
+
+  get 'videos/new'
+
+  get 'videos/create'
+
+  get 'videos/edit'
+
+  get 'videos/update'
+
+  get 'videos/destroy'
+
+  get 'content/works'
+
+  get 'content/featured_works'
+
+  get 'content/contact_us'
+
+  get 'home/index'
+
+  devise_for :admins, controllers: {
+        sessions: 'admins/sessions'
+      }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "home#index"
 end

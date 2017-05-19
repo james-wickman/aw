01fa958bf429695ccbc2283dee2807255a974997
nauticalnova <nauticalnova@hotmail.com>
2017-05-18 21:30:29 -0600

    added home page again with footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,91 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(document).on('turbolinks:load', function () {
+  $(".arrow").on('click', function() {
+      $('html,body').animate({
+          scrollTop: $(".intro_section").offset().top},
+          'slow');
+
+  });
+  $(window).on('scroll', function() {
+  	var scrollPos = $(window).scrollTop();
+  	if (scrollPos > 100) {
+  		$('.arrow').addClass('hidden');
+  	}
+  	else {
+  		$('.arrow').removeClass('hidden');
+  	}
+  });
+  $(".home_button").on('click', function() {
+      $('html,body').animate({
+          scrollTop: $(".home_section").offset().top},
+          'slow');
+  });
+  $(".intro_button").on('click', function() {
+      $('html,body').animate({
+          scrollTop: $(".intro_section").offset().top},
+          'slow');
+  });
+  $(".featured_works_button").on('click', function() {
+      $('html,body').animate({
+          scrollTop: $(".featured_works_section").offset().top},
+          'slow');
+  });
+  $(".works_button").on('click', function() {
+      $('html,body').animate({
+          scrollTop: $(".works_section").offset().top},
+          'slow');
+  });
+  $(".contact_us_button").on('click', function() {
+      $('html,body').animate({
+          scrollTop: $(".contact_us_section").offset().top},
+          'slow');
+  });
+  $(".social_media_button").on('click', function() {
+      $('html,body').animate({
+          scrollTop: $(".social_media_section").offset().top},
+          'slow');
+  });
+
+
+  var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+     isClosed = true;
+    function hamburger_cross() {
+      if (isClosed == false) {          
+        overlay.hide();
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        isClosed = true;
+      } else {   
+        overlay.show();
+        trigger.removeClass('is-closed');
+        trigger.addClass('is-open');
+        isClosed = false;
+      }
+  }
+  function run(e) {
+    $("#wrapper").toggleClass("active");
+    hamburger_cross();
+  }
+  $('[data-toggle="offcanvas"]').click(function(e) {
+      run(e);
+  });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+

5fca19b3cf711c13b9e040a9db7e33114bef4be7
nauticalnova <nauticalnova@hotmail.com>
2017-05-22 00:00:02 -0600

    added content and videos

--- Gemfile
+++ Gemfile
@@ -9,11 +9,10 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.3'
 gem 'devise'
-
 gem 'rest-client'
 gem 'mailgun-ruby'
 # Use sqlite3 as the database for Active Record
-gem 'pg'
+gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -8,22 +8,26 @@ class ContentController < ApplicationController
   def contact_us
   end
 
+
   def send_email
+
+
     respond_to do |format|
       p params
       cust_name = params[:first_name] + ' ' + params[:last_name]
       customer_email = params[:your_email]
       subject = cust_name + " " + params[:your_email]
       message = params[:message] + "!"
+      p message
       
 
       RestClient.post "https://api:key-0b4b9928196d594ec7d29f57a4f6779b"\
       "@api.mailgun.net/v3/sandbox9e4f8f9b914e4318ac4b333b40daa36b.mailgun.org/messages",
       :from => "Mailgun Sandbox <postmaster@sandbox9e4f8f9b914e4318ac4b333b40daa36b.mailgun.org>",  #  need to change too a autherized domain
-      :to => "nauticalnova@hotmail.com", # change too send_to_email when done with testing and after you have set up autherized domain.  will ned to authorize recipients, ie send to emails.
+      :to => "audiowithdrawal@gmail.com", # change too send_to_email when done with testing and after you have set up autherized domain.  will ned to authorize recipients, ie send to emails.
       :subject => "#{subject}",
       :text => "#{message}"
-      p status
+      p "status ===========" + status
       if status == 200
         format.js
       else

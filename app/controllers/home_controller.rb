b9fec4eaac2e2e998e379ad4da1ff2b5ced5f997
nauticalnova <nauticalnova@hotmail.com>
2017-07-12 18:13:05 -0600

    set basic layout for sound cloud

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
     @scoring_samples = Video.where(section: "scoring_samples")
     @featured_works = Video.where(section: "featured_works")
   end
+
   
 end
